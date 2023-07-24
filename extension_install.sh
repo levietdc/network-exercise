2f7c8e641eac19dcd4d6b15b3bb56f198e3ecf75
VIET LE <viet.le@pionero.io>
2023-07-25 00:21:43 +0700

    Network excercise

---
+++ extension_install.sh
@@ -0,0 +1,11 @@
+# ESLint
+npm install eslint --save-dev
+
+# Prettier
+npm install prettier --save-dev
+
+# ESLint plugin cho Prettier
+npm install eslint-plugin-prettier --save-dev
+
+# ESLint config cho Prettier
+npm install eslint-config-prettier --save-dev
